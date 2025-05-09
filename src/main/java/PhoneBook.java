ce41fa5237ce148d35eae84a57014dc0b7e6aa00
Левыкин Алексей Александрович <alevykin@alfabank.ru>
2025-05-10 00:26:53 +0500

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,6 @@
 public class PhoneBook {
+
+    public Integer add (String name, Long number) {
+        return null;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,32 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class PhoneBookTest {
+
+    static PhoneBook phoneBook;
+
+    @BeforeEach
+    public void init() {
+        phoneBook = new PhoneBook();
+    }
+
+    @Nested
+    class TestAdd {
+        @ParameterizedTest
+        @CsvSource({
+                "Мария, 70951112233, 1",
+                "Мария, 79221112233, 1",
+                "Петр, 73511112233, 2"
+        })
+        public void testAdd(String name, Long number, Integer recordCount) {
+            // Act
+            Integer result = phoneBook.add(name, number);
+            // Assert
+            Assertions.assertEquals(recordCount, result);
+        }
+    }
+
 }
