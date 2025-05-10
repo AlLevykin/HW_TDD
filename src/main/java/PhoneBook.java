61c899d903d1ebee8f0870c7cd443f93bc80f22f
Левыкин Алексей Александрович <alevykin@alfabank.ru>
2025-05-10 09:35:39 +0500

    Add error implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -5,6 +5,13 @@ public class PhoneBook {
     private final TreeMap<String, Long> store = new TreeMap<>();
 
     public Integer add (String name, Long number) {
+
+        if (name == null || name.isEmpty() || !name.matches("[a-zA-Zа-яА-Я]*"))
+            throw new IllegalArgumentException("Name is not valid");
+
+        if (number == null || number < 1)
+            throw new IllegalArgumentException("Number is not valid");
+
         store.put(name, number);
         return store.size();
     }
